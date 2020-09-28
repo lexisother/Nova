8a49ee344e48dfc1fbafa01f737710e8a918ee05
Lexi <lexisoth2005@gmail.com>
2020-09-28 21:19:14 +0200

    Switched to OnionLasers.
    God, Ecstar is retarded.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-import { Client } from "ecstar";
-import * as dotenv from "dotenv";
-dotenv.config();
+import {Client} from "discord.js";
+import setup from "./setup";
+import {Config} from "./core/structures";
+import {loadCommands} from "./core/command";
+import {loadEvents} from "./core/event";
 
-const client = new Client({
-    prefix: "!!",
-    disableMentions: "everyone",
-    owner: "465702500146610176"
+// This is here in order to make it much less of a headache to access the client from other files.
+// This of course won't actually do anything until the setup process is complete and it logs in.
+export const client = new Client();
+
+// Begin the command loading here rather than when it's needed like in the message event.
+setup.init().then(() => {
+	loadCommands();
+	loadEvents(client);
+    client.login(Config.token)
+        //.catch(setup.again);
 });
-
-client.login(process.env.TOKEN);
